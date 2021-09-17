3b59c4dc843a899c3a15e6aa58f2c2aba949ffc9
Jordan Felan <felan.jordan94@gmail.com>
2021-09-17 16:40:46 -0500

    covered booleans and some examples

---
+++ pythonExercises/booleans.py
@@ -0,0 +1,23 @@
+# Booleans
+
+# There are only two booleans true or false
+
+name = 'Jordan'
+is_cool = False
+
+is_cool = True
+
+#Booleans represent the idea of true and false or in coding 1 and 0
+
+# True = 1
+#False = 0
+
+# We are going to explore using booleans as conditional logic and using booleans to denote logic.
+
+#so example below would equal true
+print(bool(1))
+
+#and this example would equal false
+print(bool(0))
+
+# in those two examples above we are just converting the integers into there boolean values
